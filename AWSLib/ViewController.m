f406a56c2903066c96437bd6222b5003e2818da7
richmondwatkins <richmond@caddis.co>
2015-06-29 19:30:22 -0500

    init

---
+++ AWSLib/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AWSLib
+//
+//  Created by Richmond Watkins on 6/29/15.
+//  Copyright (c) 2015 Richmond Watkins. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ AWSStaticLib/AWSStaticLib.h
@@ -0,0 +1,13 @@
+//
+//  AWSStaticLib.h
+//  AWSStaticLib
+//
+//  Created by Richmond Watkins on 6/29/15.
+//  Copyright (c) 2015 Richmond Watkins. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AWSSNS.h>
+@interface AWSStaticLib : NSObject
+
+@end
